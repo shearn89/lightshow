23bda1bde811b7235f36b83fa03b8433dadcb840
shearn89 <alexander.shearn@gmail.com>
2016-03-06 15:57:37 +0000

    adding classes

--- controller.py
+++ controller.py
@@ -2,25 +2,68 @@
 
 import monitor.checker
 
+class Check:
+    def __init__(self, name, status, priority=0):
+        self.name = name
+        self.status = status
+        self.priority = priority
+    
+class Status:
+    def __init__(self):
+        self.checks = []
+        self.index = 0
+
+    def add(self, check):
+        self.checks.append(check)
+    
+    def __iter__(self):
+        return self
+
+    def next(self):
+        try:
+            result = self.checks[self.index]
+        except IndexError:
+            raise StopIteration
+        self.index += 1
+        return result
+
 def print_result(text, value):
     if value:
         print text + " is up"
     else:
         print text + " is down"
 
-dns_net = monitor.checker.check_dns_net()
-if dns_net:
-    print_result("dns net", dns_net)
-else:
-    print_result("net", monitor.checker.check_general_net())
-    print_result("modem", monitor.checker.check_host_port("192.168.1.254", 80))
-print_result("local DNS", monitor.checker.check_host_port("192.168.1.2", 53))
-print_result("loki", monitor.checker.check_host_via_ssh("192.168.1.2"))
-print_result("cassini", monitor.checker.check_host_via_ssh("192.168.1.252"))
-print_result("asgard_too", monitor.checker.check_host_port("192.168.1.253", 80))
-print_result("techworld", monitor.checker.check_host_port("192.168.1.252", 64004))
-print_result("loki web port", monitor.checker.check_host_port('192.168.1.2', 80))
-print_result("freya", monitor.checker.check_host_via_ssh("192.168.1.20"))
-print_result("freya web port", monitor.checker.check_host_port('192.168.1.20', 80))
+def run_checks(status):
+    dns_net = Check('Name Based Internet',monitor.checker.check_dns_net())
+    status.add(dns_net)
+    ip_net = Check('IP Based Internet',monitor.checker.check_general_net())
+    status.add(ip_net)
+    modem = Check('Modem',monitor.checker.check_host_port("192.168.1.254", 80))
+    status.add(modem)
+    
+    dns = Check('Local DNS', monitor.checker.check_host_port("192.168.1.2", 53))
+    status.add(dns)
+    loki = Check('Loki', monitor.checker.check_host_via_ssh("192.168.1.2"))
+    status.add(loki)
+    loki_web = Check('Loki Web Server', monitor.checker.check_host_port('192.168.1.2', 80))
+    status.add(loki_web)
+    
+    ap_two = Check('Asgard_Too Wifi', monitor.checker.check_host_port("192.168.1.253", 80))
+    status.add(ap_two)
+    
+    cassini = Check('Cassini', monitor.checker.check_host_via_ssh("192.168.1.252"))
+    status.add(cassini)
+    techworld = Check('TechWorld Server', monitor.checker.check_host_port("192.168.1.252", 64004))
+    status.add(techworld)
+    
+    freya = Check('Freya', monitor.checker.check_host_via_ssh("192.168.1.20"))
+    status.add(freya)
+    freya_web = Check('Freya Web Server', monitor.checker.check_host_port('192.168.1.20', 80))
+    status.add(freya_web)
 
-# These should be in priority order: if net is down,
+if __name__ == "__main__":
+    print "running checks..."
+    health_status = Status()
+    run_checks(health_status)
+    for check in health_status:
+        print "%s: %s" % (check.name, 'up' if check.status else 'down')
