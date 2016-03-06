71e3abe267ca3fa7c1f4da32aceb42041605e25c
shearn89 <alexander.shearn@gmail.com>
2016-03-06 19:15:25 +0000

    working with lighttpd

--- controller.py
+++ controller.py
@@ -1,18 +1,14 @@
-# This class should control the output of checks and trigger different light modes.
-
 import os
 import time
 import datetime
 import signal
 import sys
-import threading
 
-from twisted.web import server, resource
-from twisted.internet import reactor,endpoints
-
+from jinja2 import Environment, FileSystemLoader
 from monitor import checks,status
 
-STATUS = "/dev/shm/monitor-status.txt"
+STATUS = "/var/www/html/index.html"
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def run_checks(status):
     dns_net = checks.Check('Name Based Internet',checks.check_dns_net())
@@ -42,44 +38,22 @@ def run_checks(status):
     freya_web = checks.Check('Freya Web Server', checks.check_host_port('192.168.1.20', 80))
     status.add(freya_web)
 
-### Checks setup
-class checkThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.health_status = status.Status()
-        self.signal = True
-    
-    def run(self):
-        print "running checks..."
-        run_checks(self.health_status)
-        ## Lock this?
-        ostring = "%s\n" % datetime.datetime.now()
-        print "building string"
-        for check in self.health_status:
-            ostring += "%s: %s\n" % (check.name, 'up' if check.status else 'down')
-        print "built"
-        with open(STATUS, 'w') as output:
-            output.write(ostring)
-        print "written"
-### end checks setup
-
-class MonServer(resource.Resource):
-    isLeaf = True
-    numberRequests = 0
-
-    def render_GET(self, request):
-        self.numberRequests += 1
-        request.setHeader(b"content-type", b"text/plain")
-        content = u"I am request #{}\n".format(self.numberRequests)
-        return content.encode("ascii")
 
 if __name__ == "__main__":
-    tmanager = []
-    t1 = checkThread()
-    t1.start()
+    health_status = status.Status()
 
-    endpoints.serverFromString(reactor, "tcp:8080").listen(server.Site(MonServer()))
-    print "starting twisted server"
-    reactor.run()
-    print "joining sleep thread"
-    t1.join()
+    j2_env = Environment(loader=FileSystemLoader(THIS_DIR), trim_blocks=True)
+    while True:
+        try:
+            run_checks(health_status)
+            output_list = []
+            # ostring = "%s\n" % datetime.datetime.now()
+            tstamp = datetime.datetime.now()
+            for check in health_status:
+                output_list.append({'name': check.name, 'status': check.status})
+            with open(STATUS, 'w') as output:
+                output.write(j2_env.get_template('template.html').render(last_check = tstamp,
+                    content=output_list))
+            time.sleep(30)
+        except KeyboardInterrupt:
+            break
